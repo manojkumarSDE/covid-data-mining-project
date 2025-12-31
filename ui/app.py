62feaf43416d1011db56baf4ef2d931103b62bf8
Manoj Kumar challa <manojkumar2684@gmail.com>
2025-12-31 14:55:53 +0530

    Add files via upload

--- ui/app.py
+++ ui/app.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import pandas as pd
 
+st.title("COVID-19 Data Mining Dashboard")
+
+clustered = pd.read_csv("../outputs/clustered_data.csv")
+age_data = pd.read_csv("../outputs/age_mortality.csv", header=None)
+
+st.header("Top 10 Affected Countries")
+st.table(clustered.sort_values(by="Confirmed", ascending=False).head(10))
+
+st.header("Mortality Rate by Age Group")
+st.bar_chart(age_data.set_index(0))
